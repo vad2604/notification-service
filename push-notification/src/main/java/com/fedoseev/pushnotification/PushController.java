63d79e4f025e206610bf779341ea708350f7ba7d
vad2604 <vad2604@yandex.ru>
2020-05-20 09:54:33 +0400

    Add Eureka dependency to config server

--- push-notification/src/main/java/com/fedoseev/pushnotification/PushController.java
+++ push-notification/src/main/java/com/fedoseev/pushnotification/PushController.java
@@ -3,10 +3,7 @@ package com.fedoseev.pushnotification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -17,7 +14,7 @@ public class PushController {
     private static final Logger logger = LoggerFactory.getLogger(PushMessage.class);
     private static final String SUCCESS_RESPONSE = "Push successfully sent";
 
-    @GetMapping("/getPush")
+    @PostMapping("/sendPush")
     public String sendPush(@Valid @RequestBody PushMessage pushMessage) {
         logger.info(SUCCESS_RESPONSE);
         return SUCCESS_RESPONSE;
